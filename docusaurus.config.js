62f8683451fc96f1ffb9753c7779af1f7c8045a1
Yi Ming <ofseed@foxmail.com>
2023-02-22 19:00:49 +0800

    docs: change logo and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
         title: "My Site",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/logo.png",
         },
         items: [
           {
