8be450c73d6ec11c547573ad151906223134bedf
Yi Ming <ofseed@foxmail.com>
2023-02-23 20:18:29 +0800

    feat: remove tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,6 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "REASONER",
-  tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
