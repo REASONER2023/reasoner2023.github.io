59aec4ace0c5a42fec74fffd9f5f9da3cb48b62b
Yi Ming <ofseed@foxmail.com>
2023-02-23 20:21:11 +0800

    docs: update meta description

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,15 +10,15 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://reasoner2023.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "REASONER2023", // Usually your GitHub org/user name.
+  projectName: "reasoner2023.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
