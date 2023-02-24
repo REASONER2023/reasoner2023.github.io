2f2a3dae1e8caf45c337f6291c02719ff5c05319
Yi Ming <ofseed@foxmail.com>
2023-02-24 11:49:26 +0800

    feat: basic home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,102 @@
 import React from "react";
 import clsx from "clsx";
+import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
+import Translate from "@docusaurus/Translate";
 
-import styles from "./index.module.css";
+const features = [
+  {
+    title: "Multi-modal Explanations",
+    imageUrl: "img/explanations.png",
+    description: "Feature description",
+  },
+  {
+    title: "Multi-aspect Explanation Ground Truth",
+    imageUrl: "img/truth.png",
+    description: "Feature description",
+  },
+  {
+    title: "Explanable toolkit",
+    imageUrl: "img/toolkit.png",
+    description: "Feature description",
+  },
+  {
+    title: "Three thousand Real User feedback",
+    imageUrl: "img/feedback.png",
+    description: "Feature description",
+  },
+];
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/dataset"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <div className={clsx("col col--6", styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
-      </div>
-    </header>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
   );
 }
 
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig: { customFields = {}, tagline } = {} } = context;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
+    <Layout title={tagline} description={customFields.description as string}>
+      <div className={styles.hero}>
+        <div className={styles.heroInner}>
+          <h1 className={styles.heroProjectTagline}>
+            <img
+              alt="Reasoner logo"
+              className={styles.heroLogo}
+              src={useBaseUrl("img/logo.png")}
+            />
+            <span className={styles.heroTitleTextHtml}>
+              <Translate
+                id="homepage.hero.title"
+                description="Home page hero title, can contain simple html tags"
+                values={{
+                  explainable: <b>Explainable</b>,
+                  multi_aspect: <b>Multi-aspect</b>,
+                  ground_truth: <b>Ground Truth</b>,
+                }}
+              >
+                {`An {explainable} Recommendation Dataset with {multi_aspect} Real User Labeled {ground_truth}`}
+              </Translate>
+            </span>
+          </h1>
+          <div className={styles.indexCtas}>
+            <Link
+              className={clsx("margin-left--md", styles.indexTryMeButton)}
+              to="https://github.com/REASONER2023/reasoner2023.github.io"
+            >
+              View on GitHub
+            </Link>
+          </div>
+        </div>
+      </div>
       <main>
-        <HomepageFeatures />
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
       </main>
     </Layout>
   );
 }
+
+export default Home;
